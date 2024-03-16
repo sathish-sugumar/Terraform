bb7d4a98706336b6f7ac2c2d4233ccde6da57c82
sathish_s <sathishssiam@gmail.com>
2024-03-16 12:27:55 +0530

    Added Sercurity Group to EC2

--- AWS_EC2.tf
+++ AWS_EC2.tf
@@ -9,15 +9,63 @@ terraform {
 
 provider "aws" {
  
-  region="ap-southeast-1"
+  region="ap-southeast-1"  # region configuration
   # Configuration options
 }
+
+
+# creating instance
 resource "aws_instance" "terraform" {
 
-  ami     = "ami-ami-015f72d56355ebc27" 
+
+  count = 2              # will create 2 instance
+  ami     = "ami-015f72d56355ebc27" # amazon linux machine
   instance_type = "t2.micro"
-  
+  security_groups = [aws_security_group.MySG.name]  # using the sercurity group calling as MySG
+
   tags = { 
     name= "myinstance"
   }
 }
+
+# creating security group name as MYSG
+resource "aws_security_group" "MySG" {
+  
+  
+  name        = "MySG1"
+  description = "Allow SSH  traffic only "
+ 
+
+  ingress {
+    description      = "setting inbound rule  protocol SSG port 22 "
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+   # ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+  
+  ingress {
+    description      = "setting inbound rule  protocol  http  port  80 "
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+   # ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    #ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "MYSG"
+  }
+  
+}
+
+
